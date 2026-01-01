ad71ef983f2a3faabc113994f23f3d58f64e97f3
Biswajitn23 <nbiswajit978@gmail.com>
2026-01-01 13:31:07 +0530

    fix: relax typings for calendar ics

--- api/calendar.ts
+++ api/calendar.ts
@@ -39,7 +39,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const snapshot = await db.collection('events').get();
 
     const events = snapshot.docs
-      .map((doc) => ({ id: doc.id, ...doc.data() }))
+      .map((doc) => ({ id: doc.id, ...(doc.data() as any) }))
       .filter((event) => !!event.date)
       .sort((a, b) => {
         const aDate = toDate(a.date)?.getTime() ?? Number.POSITIVE_INFINITY;
