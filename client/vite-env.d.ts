3ce802afe66f5ff39a070e28170f89511e537725
Biswajitn23 <nbiswajit978@gmail.com>
2026-01-03 18:30:23 +0530

    Add event email template and update EmailJS template IDs for welcome and event notifications

--- client/vite-env.d.ts
+++ client/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_HCAPTCHA_SITEKEY: string;
+  readonly VITE_PUBLIC_BUILDER_KEY: string;
+  // Add more env variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
